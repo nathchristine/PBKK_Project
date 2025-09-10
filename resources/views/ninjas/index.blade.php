3a7ca4d9568c24aa36726cb831b7d91fd73e4c59
Nathaniel Christine <simanullangnathaniel@gmail.com>
2025-09-10 09:49:23 +0700

    Added Blade Directives, Layouts, Slots, Components, Attributes, Props, CSS, and Tailwind

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -1,24 +1,13 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ninja Network | Home</title>
-</head>
-<body>
+<x-layout>
     <h2>Currently Available Ninjas</h2>
-    <p>{{ $greeting }}</p>
 
     <ul>
-        <li>
-            <a href="/ninjas/{{ $ninjas[0]['id'] }}">
-                {{ $ninjas[0]["name"] }} 
-            </a>
-        </li>
-         <li>
-            <a href="/ninjas/{{ $ninjas[1]['id'] }}">
-                {{ $ninjas[1]["name"] }} 
-            </a>
-        </li>
+        @foreach($ninjas as $ninja)
+            <li>
+                <x-card href="/ninjas/{{ $ninja['id'] }}" :highlight="$ninja['skill'] > 70">
+                    <h3>{{ $ninja['name'] }}</h3>
+                </x-card>
+            </li>
+        @endforeach
     </ul>
-</body>
-</html>
+</x-layout>
